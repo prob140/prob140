6169b82fc2740c4bd67ddf8edd5d908ceab50e58
Jason Zhang <zhang.j@berkeley.edu>
2016-11-24 18:25:08 -0800

    updates to package

--- prob140/__init__.py
+++ prob140/__init__.py
@@ -1,1 +1,2 @@
 from .prob140 import *
+from .version import *
--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@ import sys
 from distutils.core import setup
 
 
-version = 0.1
-
 if sys.version_info < (3, 0):
     raise ValueError('This package requires python >= 3.0')
 
 with open('requirements.txt') as fid:
     install_requires = [l.strip() for l in fid.readlines() if l]
+
+with open('prob140/version.py') as fid:
+    for line in fid:
+        if line.startswith('__version__'):
+            version = line.strip().split()[-1][1:-1]
+            break
 
 
 setup(
@@ -20,7 +24,7 @@ setup(
     author = 'Jason Zhang, Dibya Ghosh',
     author_email = 'zhang.j@berkeley.edu',
     url = 'https://probability.gitlab.io/instructors/',
-    download_url = 'https://gitlab.com/probability/prob140',
+    download_url = 'https://gitlab.com/probability/prob140/raw/master/dist/prob140-%s.tar.gz' % version,
     keywords = ['data', 'probability', 'berkeley', 'Prob140'],
     classifiers = []
 )
