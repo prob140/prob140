085641f5f7aeb8787233576285026fac6a0348da
Jason Zhang <zhang.j@berkeley.edu>
2017-03-08 15:38:27 -0800

    Plot_continuous now works with any univariate density function

--- prob140/plots.py
+++ prob140/plots.py
@@ -3,13 +3,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def Plot_continuous(func, *args, **kwargs):
+def Plot_continuous(func, x_limits, *args, **kwargs):
+    """
 
-    if "x_limits" in kwargs:
-        interval = kwargs.pop("x_limits")
-    else:
-        interval = args[0]
-        args = args[1:]
+    Parameters
+    ----------
+    func : function or str
+        Univariate density function or a String for the scipy dist name
+    x_limits : iterable
+        Array, list, or tuple of size 2, containing the lower and upper bound to be plotted
+    args : floats (optional)
+        Arguments of the distribution if func was a String.
+
+    Optional Named Parameters
+    -------------------------
+    tails : boolean (optional)
+        If True, left_end will shade from the lower bound up to left_end, and right_end will shade from right_end up to
+        the upper bound. If False, left_end will shade to right_end. (Default: False)
+    left_end : float (optional)
+        Left side of event to be shaded (Default: None)
+    right_end : float (optional)
+        Right side of event to be shaded (Default: None)
+    cdf : boolean (optional)
+        If True and func was string, the cdf will be plotted (Default: False)
+
+    All pyplot named arguments (such as color) should work as well. See http://matplotlib.org/api/pyplot_api.html#matplotlib.pyplot.plot
+
+    Returns
+    -------
+    None
+
+    """
+
+    interval = x_limits
 
     if isinstance(func, str):
         func = func.lower()
@@ -59,19 +85,19 @@ def Plot_continuous(func, *args, **kwargs):
 
 
 def Plot_expon(x_limits, lamb, **kwargs):
-    Plot_continuous("expon", 0, lamb, x_limits=x_limits, **kwargs)
+    Plot_continuous("expon", x_limits, 0, lamb, **kwargs)
 
 
 def Plot_norm(x_limits, mu, sigma, **kwargs):
-    Plot_continuous("norm", mu, sigma, x_limits=x_limits, **kwargs)
+    Plot_continuous("norm", x_limits, mu, sigma, **kwargs)
 
 
 def Plot_arcsine(x_limits, **kwargs):
-    Plot_continuous("arcsine", x_limits=x_limits, **kwargs)
+    Plot_continuous("arcsine", x_limits, **kwargs)
 
 
 def Plot_beta(x_limits, a, b, **kwargs):
-    Plot_continuous("beta", a, b, x_limits=x_limits, **kwargs)
+    Plot_continuous("beta", x_limits, a, b, **kwargs)
 
 
 def Plot_cauchy(x_limits, **kwargs):
@@ -79,44 +105,44 @@ def Plot_cauchy(x_limits, **kwargs):
 
 
 def Plot_chi2(x_limits, df, **kwargs):
-    Plot_continuous("chi2", df, x_limits=x_limits, **kwargs)
+    Plot_continuous("chi2", x_limits, df, **kwargs)
 
 
 def Plot_erlang(x_limits, r, lamb, **kwargs):
-    Plot_continuous("erlang", r, 0, 1 / lamb, x_limits=x_limits, **kwargs)
+    Plot_continuous("erlang", x_limits, r, 0, 1 / lamb, **kwargs)
 
 
 def Plot_f(x_limits, dfn, dfd, **kwargs):
-    Plot_continuous("f", dfn, dfd, x_limits=x_limits, **kwargs)
+    Plot_continuous("f", x_limits, dfn, dfd, **kwargs)
 
 
 def Plot_gamma(x_limits, r, lamb, **kwargs):
-    Plot_continuous("gamma", r, 0, 1 / lamb, x_limits=x_limits, **kwargs)
+    Plot_continuous("gamma", x_limits, r, 0, 1 / lamb, **kwargs)
 
 
 def Plot_lognorm(x_limits, mu, sigma, **kwargs):
-    Plot_continuous("lognorm", sigma, 0, np.exp(mu), x_limits=x_limits, **kwargs)
+    Plot_continuous("lognorm", x_limits, sigma, 0, np.exp(mu), **kwargs)
 
 
 def Plot_pareto(x_limits, alpha, **kwargs):
-    Plot_continuous("pareto", alpha, x_limits=x_limits, **kwargs)
+    Plot_continuous("pareto", x_limits, alpha, **kwargs)
 
 
 def Plot_powerlaw(x_limits, a, **kwargs):
-    Plot_continuous("powerlaw", a, x_limits=x_limits, **kwargs)
+    Plot_continuous("powerlaw", x_limits, a, **kwargs)
 
 
 def Plot_rayleigh(x_limits, sigma, **kwargs):
-    Plot_continuous("rayleigh", 0, sigma, x_limits=x_limits, **kwargs)
+    Plot_continuous("rayleigh", x_limits, 0, sigma, **kwargs)
 
 
 def Plot_t(x_limits, df, **kwargs):
-    Plot_continuous("t", df, x_limits=x_limits, **kwargs)
+    Plot_continuous("t", x_limits, df, **kwargs)
 
 
 def Plot_triang(x_limits, a, b, c, **kwargs):
-    Plot_continuous("triang", (c - a) / (b - a), a, b - a, x_limits=x_limits, **kwargs)
+    Plot_continuous("triang", x_limits, (c - a) / (b - a), a, b - a, **kwargs)
 
 
 def Plot_uniform(x_limits, a, b, **kwargs):
-    Plot_continuous("uniform", a, b - a, x_limits=x_limits, **kwargs)
+    Plot_continuous("uniform", x_limits, a, b - a, **kwargs)
